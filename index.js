db68a3362e969ba2a1a4a1a9efa4b7b723d18e7c
david-machek_dhl <david.machek@dhl.com>
2024-08-30 08:53:26 +0200

    update logging

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const dotenv = require('dotenv').config();
 
     try {
         const stats = await meteoriteController.getMeteoriteStatistics();
+        console.log('Meteorite stats fetched!'.bgGreen + '\n');
         meteoriteController.displayData(stats);
     } catch (error) {
         console.error(`Failed to get meteorite stats: ${error}`.red);
